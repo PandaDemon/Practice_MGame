c7396f5c48d53e9e81948009f8e143093dc34402
PandaDemon <mpanukov@gmail.com>
2018-02-07 20:21:23 +0200

    CS script
    at now we write scripts on C#

---
+++ 2D_MGame/Assets/Logic.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logic : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log("FixUpdate time !" + Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Debug.Log("Fix time !" + Time.deltaTime);
+	}
+}
