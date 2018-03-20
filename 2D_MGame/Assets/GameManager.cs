4e084f9f70561a97f751264483262f2ed10315b1
PandaDemon <mpanukov@gmail.com>
2018-03-20 17:57:16 +0200

    Update Unity

---
+++ 2D_MGame/Assets/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public Question[] questions;
+
+    void Start()
+    {
+        
+    }
+}
--- 2D_MGame/Assets/Scripts/Gamefunction.cs
+++ 2D_MGame/Assets/Scripts/Gamefunction.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Gamefunction : MonoBehaviour {
+public class GameFunction : MonoBehaviour {
 
 	public void ExitGame()
     {
